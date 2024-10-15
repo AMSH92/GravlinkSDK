294eb0b74e9f8f7ef629efae8800eb8bbc0a81e9
Abdullah <alsharif.dev@gmail.com>
2024-10-15 20:58:12 +0300

    fixes

--- Package.swift
+++ Package.swift
@@ -13,6 +13,7 @@ let package = Package(
         ),
     ],
     dependencies: [
+        // Add any other dependencies required by your package.
         .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.10.0"),
         .package(url: "https://github.com/stasel/WebRTC.git", from: "129.0.0"),
         .package(url: "https://github.com/socketio/socket.io-client-swift.git", branch: "master")
@@ -21,16 +22,6 @@ let package = Package(
         .binaryTarget(
             name: "GravlinkBinary",
             path: "./Sources/GlinkFramwork.xcframework"
-        ),
-        .target(
-            name: "GravlinkExtras",
-            dependencies: [
-                "Alamofire",
-                "WebRTC",
-                .product(name: "SocketIO", package: "socket.io-client-swift"),
-                "GravlinkBinary"
-            ],
-            path: "./Sources"
         )
     ]
 )
