f4cfe9dc4519d493451753ba0776e82d6a9642fb
Abdullah <alsharif.dev@gmail.com>
2024-10-15 20:34:23 +0300

    0.1

--- Package.swift
+++ Package.swift
@@ -1,23 +1,36 @@
-// swift-tools-version: 5.10
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
+// swift-tools-version:5.7
 import PackageDescription
 
 let package = Package(
     name: "GravlinkSDK",
+    platforms: [
+        .iOS(.v13)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "GravlinkSDK",
-            targets: ["GravlinkSDK"]),
+            targets: ["GravlinkBinary"]
+        ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.10.0"),
+        .package(url: "https://github.com/stasel/WebRTC.git", from: "129.0.0"),
+        .package(url: "https://github.com/socketio/socket.io-client-swift.git", branch: "master")
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
+        .binaryTarget(
+            name: "GravlinkBinary",
+            path: "./Sources/GlinkFramwork.xcframework"
+        ),
         .target(
-            name: "GravlinkSDK"),
-        .testTarget(
-            name: "GravlinkSDKTests",
-            dependencies: ["GravlinkSDK"]),
+            name: "GravlinkExtras",
+            dependencies: [
+                "Alamofire",
+                "WebRTC",
+                .product(name: "SocketIO", package: "socket.io-client-swift"),
+                "GravlinkBinary"
+            ],
+            path: "./Sources"
+        )
     ]
 )
--- Sources/GravlinkSDK/GravlinkSDK.swift
+++ Sources/GravlinkSDK/GravlinkSDK.swift
@@ -2,9 +2,27 @@
 // https://docs.swift.org/swift-book
 
 import Foundation
+import GravlinkSDK
 
 public struct SDK {
+    private static var isAPIKeyValid = false
+    
+  
+    public static func configure(apiKey: String) {
+        
+    }
+    
+    private static func checkAPIKey() -> Bool {
+        if !isAPIKeyValid {
+            print("Please Provide API KEY")
+        }
+        return isAPIKeyValid
+    }
+    
     public static func start() {
+        guard checkAPIKey() else {return}
         print("Hello WORLD")
     }
+    
+    
 }
