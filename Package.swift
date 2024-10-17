c30ef2aa6b24d7dd487668ac4377466a52d3b3d0
Abdullah <alsharif.dev@gmail.com>
2024-10-17 14:01:28 +0300

    fix

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.7
+// swift-tools-version:5.5
 import PackageDescription
 
 let package = Package(
@@ -6,25 +6,22 @@ let package = Package(
     platforms: [
         .iOS(.v13)
     ],
-    
     products: [
-        // Expose the binary framework as the product of the package
         .library(
             name: "GravlinkSDK",
-            targets: ["GravlinkSDK"]
+            targets: ["GlinkFramework"] // Only include the binary framework here
         ),
     ],
     dependencies: [
-        // Add any other dependencies required by your package.
-        .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.10.0"),
-        .package(url: "https://github.com/stasel/WebRTC.git", from: "129.0.0"),
-        .package(url: "https://github.com/socketio/socket.io-client-swift.git", branch: "master")
+        // Add external dependencies here if needed by the framework
+        .package(url: "https://github.com/Alamofire/Alamofire", from: "5.4.0"),
+        .package(url: "https://github.com/stasel/WebRTC.git", from: "94.0.0"),
+        .package(url: "https://github.com/socketio/socket.io-client-swift", branch: "master"),
     ],
     targets: [
-        // Binary target pointing to the XCFramework
         .binaryTarget(
-            name: "GravlinkSDK",
-            path: "./Sources/GlinkFramwork.xcframework"
+            name: "GlinkFramework",  // Matches the framework and binary name
+            path: "./build/GlinkFramework.xcframework"  // Path to the .xcframework
         )
     ]
 )
